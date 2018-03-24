bc97a0175051f613562a69b79437845e06580591
Juampy <juampynr@gmail.com>
2018-03-24 15:42:35 +0100

    Add settings.php to version control and rely on settings.local.php

--- .travis/config/settings.local.php
+++ .travis/config/settings.local.php
@@ -13,3 +13,4 @@ $databases['default']['default'] = array (
 
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = 'sites/default/files/config_KaEbND6z9WvIBDnApD_YmTrI42ZryedG2lAUC30RFvx8I_7eU9TcvwMhFk8d-_GO-Snp9y5YwA/sync';
+$settings['hash_salt'] = 'MuobC_x0x3Cmw9gG6X366aMTR0CF_P8a4iK4rz4iKoLHYU7pwEDSldx5YdX6tDDBJcr1JICqrw';
